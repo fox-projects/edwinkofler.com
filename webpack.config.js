7df066d0f0cb763b3b58c672a7ee8651202fb539
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-06-17 11:25:24 -0700

    feat: create build workflow

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+let path = require('path')
+let HtmlWebpackPlugin = require('html-webpack-plugin')
+let MiniCssExtractPlugin = require('mini-css-extract-plugin')
+let { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+let dev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  mode: dev ? 'development' : 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          dev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              localsConvention: 'camelCaseOnly'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    ... dev ? [] : [ new MiniCssExtractPlugin() ],
+    ... dev ? [] : [ new CleanWebpackPlugin() ]
+  ]
+}
